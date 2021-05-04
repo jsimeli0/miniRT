697e0929cd46b7d2667db73400f93735902d941f
Jorge luis Simelio jurado <jsimelio@f1r2s16.codam.nl>
2021-05-04 20:25:43 +0200

    eod

---
+++ geometry.h
@@ -0,0 +1,13 @@
+typedef	struct		s_Ray
+{
+	uint32_t	origin[3];
+	uint32_t vector[3];
+}					t_Ray;
+
+typedef	struct		s_Options
+{
+	uint32_t	width;
+	uint32_t	height;
+	uint32_t	fov;
+}					t_Options;
+
